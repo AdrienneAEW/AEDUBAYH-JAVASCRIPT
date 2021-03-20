d978b33c1c310b6394245394d36577c346cf2f72
Adrienne Warden <adrienneaew@gmail.com>
2021-03-20 16:25:57 -0400

    End02-07

---
+++ Zell Lessons/LearnJS-script.js
@@ -0,0 +1,5 @@
+
+document.getElementsByClassName("js-test1")[0].innerHTML = "You\'re going to need to open the console for next lesson babycakes!"
+
+const nextLesson = "02-08 Functions" 
+    console.log("The next lesson is " + nextLesson)
